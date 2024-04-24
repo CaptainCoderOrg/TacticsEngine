4bb5409345616b09e52ae4e43ed481e1ccab6668
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-04-23 18:19:25 -0700

    feat: BoardEditor should save and load board.

--- WebEditor/Program.cs
+++ WebEditor/Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using WebEditor;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<WebEditor.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorDownloadFile();
 
 await builder.Build().RunAsync();
