91b87f569cab6b4255a0c47894162673c09f72b2
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-04-29 11:11:36 -0700

    refactor: Simplify FigureRenderer / Tool

--- WebEditor/Components/BoardEditor/Tools/FigureTool.cs
+++ WebEditor/Components/BoardEditor/Tools/FigureTool.cs
@@ -4,42 +4,38 @@ namespace WebEditor.Components.BoardEditor;
 public sealed class FigureTool : Tool
 {
     public static FigureTool Shared { get; } = new();
-    public Figure ToDraw { get; private set; } = new() { Width = 1, Height = 2 };
-    public Positioned<Figure>? Selected { get; private set; }
-    public Positioned<Figure>? Target { get; private set; }
+    private Positioned<Figure>? _selected;
+    public Positioned<Figure>? DraggedFigure { get; private set; }
     private Position _offset = new();
-    public bool IsDragging { get; private set; } = false;
 
     public override void OnSelectFigure(Board board, Positioned<Figure> figure)
     {
-        Selected = figure;
+        _selected = figure;
     }
 
     public override void OnStartDragFigure(Board board, Positioned<Figure> figure, Position offset)
     {
         _offset = offset;
-        IsDragging = true;
-        Selected = figure;
+        _selected = figure;
         board.RemoveFigure(figure.Position);
     }
 
     public override void OnMouseOver(Board board, Position position)
     {
         base.OnMouseOver(board, position);
-        if (IsDragging && Selected is Positioned<Figure> figure)
+        if (_selected is Positioned<Figure> figure)
         {
-            Target = new(figure.Element, position + _offset);
+            DraggedFigure = new(figure.Element, position + _offset);
         }
     }
 
     public override void OnMouseUp(Board board, Position endPosition)
     {
-        if (IsDragging && Selected != null && !board.TryAddFigure(endPosition + _offset, Selected.Element))
+        if (_selected != null && !board.TryAddFigure(endPosition + _offset, _selected.Element))
         {
-            board.Figures.Add(Selected);
+            board.Figures.Add(_selected);
         }
-        Selected = null;
-        Target = null;
-        IsDragging = false;
+        _selected = null;
+        DraggedFigure = null;
     }
 }
