7ef66645bc7f2738b04612bd75eff7fc979946d6
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-01 12:16:42 -0700

    chore: Refactor namespaces.

--- WebEditor/Program.cs
+++ WebEditor/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using WebEditor.Components.BoardEditor;
+using WebEditor.Tools;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<WebEditor.App>("#app");
