a87e7f58eb9651ad982b3b5f3e08e16d581cbace
smabuk <2011834+smabuk@users.noreply.github.com>
2024-05-02 11:48:21 +0100

    Change Position from a record struct to a record class
    * Simplifies creating a "NoPosition" position
    
    Could have created a factory and extensions to create and compare but the struct has other implications as well in the functional model

--- Board/Position.cs
+++ Board/Position.cs
@@ -1,5 +1,7 @@
 namespace CaptainCoder.TacticsEngine.Board;
-public record struct Position(int X, int Y)
+public record Position(int X, int Y)
 {
     public static Position operator +(Position a, Position b) => new(a.X + b.X, a.Y + b.Y);
 }
+
+public record NoPosition() : Position(int.MinValue, int.MinValue);
