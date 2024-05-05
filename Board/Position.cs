a645d1eac6db78337068f5af90a6062ccf6917c1
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-05 10:08:04 -0700

    feat: Add subtraction operator to Postion.

--- Board/Position.cs
+++ Board/Position.cs
@@ -2,4 +2,5 @@ namespace CaptainCoder.TacticsEngine.Board;
 public record struct Position(int X, int Y)
 {
     public static Position operator +(Position a, Position b) => new(a.X + b.X, a.Y + b.Y);
+    public static Position operator -(Position a, Position b) => new(a.X - b.X, a.Y - b.Y);
 }
