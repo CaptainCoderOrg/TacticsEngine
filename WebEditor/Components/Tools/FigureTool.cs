b8a39bed9263373d9847285adfea0500d1f3366c
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-06 10:43:41 -0700

    feat: Add FigureTool.OnChange event.

--- WebEditor/Components/Tools/FigureTool.cs
+++ WebEditor/Components/Tools/FigureTool.cs
@@ -4,5 +4,18 @@ namespace WebEditor.Tools;
 public sealed class FigureTool : Tool
 {
     public static FigureTool Shared { get; } = new();
-    public Positioned<Figure>? Selected { get; set; }
+    private Positioned<Figure>? _selected;
+    public Positioned<Figure>? Selected
+    {
+        get => _selected;
+        set
+        {
+            if (_selected == value) { return; }
+            var previous = _selected;
+            _selected = value;
+            OnChange?.Invoke(previous, _selected);
+        }
+    }
+
+    public event Action<Positioned<Figure>?, Positioned<Figure>?>? OnChange;
 }
