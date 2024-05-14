12d420b11ba3f5a8fb944c796468fc319a90d81e
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-14 09:54:18 -0700

    chore: Remove unused Tool code.

--- WebEditor/Program.cs
+++ WebEditor/Program.cs
@@ -2,14 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using WebEditor.Components.DragAndDrop;
-using WebEditor.Tools;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<WebEditor.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton(ToolManager.Shared);
 builder.Services.AddSingleton(DragAndDropManager.Shared);
 
 builder.Services.AddBlazorDownloadFile();
