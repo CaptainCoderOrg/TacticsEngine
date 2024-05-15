87e966990c0d1cef244e5f55dd14a906dc1fc32d
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-15 11:02:09 -0700

    feat: Partial implementation of ClipboardRenderer

---
+++ WebEditor/Components/Board/SelectTilesTool.cs
@@ -0,0 +1,18 @@
+﻿
+using CaptainCoder.TacticsEngine.Board;
+
+using WebEditor.Components.Board.Clipboard;
+
+namespace WebEditor.Components.Board;
+
+public sealed record SelectTilesTool(Position Start, BoardRenderer Target, ClipboardRenderer Clipboard) : BoundingBoxTool(Start, Target)
+{
+    public override string ColorClass => "select-tiles";
+
+    public override void OnClick(Position position) { }
+
+    public override void OnDrop(Position position)
+    {
+        Clipboard.CurrentSelection = Selection;
+    }
+}
