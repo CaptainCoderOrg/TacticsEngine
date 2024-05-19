910611c0f195d16f185a42d6ecbc8c7c5fc7725f
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-19 09:12:53 -0700

    feat: Implement placement preview for moving a selection.

---
+++ WebEditor/Components/Board/Clipboard/MoveSelectionTool.cs
@@ -0,0 +1,36 @@
+﻿using CaptainCoder.TacticsEngine.Board;
+
+namespace WebEditor.Components.Board.Clipboard;
+
+public sealed class MoveSelectionTool(Position mousePosition) : ITool
+{
+    public required BoardData Selection { get; init; }
+    public required BoardRenderer Parent { get; init; }
+    public required Position SelectionOffset { get; init; }
+    public required Position MouseOffset { get; init; }
+    private Position _mousePosition = mousePosition;
+    public Position MousePosition => _mousePosition - MouseOffset;
+    public Type ComponentType { get; } = typeof(MoveSelectionRenderer);
+
+    private Dictionary<string, object>? _parameters;
+    public Dictionary<string, object> ComponentParameters
+    {
+        get
+        {
+            if (_parameters is null)
+            {
+                _parameters = new() { { nameof(MoveSelectionRenderer.Tool), this } };
+            }
+            return _parameters;
+        }
+    }
+
+    public void OnClick(Position position) { }
+
+    public void OnDragOver(Position position)
+    {
+        _mousePosition = position;
+    }
+
+    public void OnDrop(Position position) { }
+}
