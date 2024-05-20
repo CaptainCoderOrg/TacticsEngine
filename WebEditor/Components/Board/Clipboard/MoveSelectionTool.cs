b9a0b0c709c1916a1c91957a7f494eb015332098
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-20 05:27:15 -0700

    feat: Implement MoveSelectionTool.OnDrop - Cut/Paste from original position.

--- WebEditor/Components/Board/Clipboard/MoveSelectionTool.cs
+++ WebEditor/Components/Board/Clipboard/MoveSelectionTool.cs
@@ -4,15 +4,10 @@ namespace WebEditor.Components.Board.Clipboard;
 
 public sealed class MoveSelectionTool(Position mousePosition) : ITool
 {
-    public required BoardData Selection { get; init; }
-    public required BoardRenderer Parent { get; init; }
-    public required Position SelectionOffset { get; init; }
-    public required Position MouseOffset { get; init; }
-    private Position _mousePosition = mousePosition;
-    public Position MousePosition => _mousePosition - MouseOffset;
     public Type ComponentType { get; } = typeof(MoveSelectionRenderer);
 
     private Dictionary<string, object>? _parameters;
+
     public Dictionary<string, object> ComponentParameters
     {
         get
@@ -25,6 +20,14 @@ public sealed class MoveSelectionTool(Position mousePosition) : ITool
         }
     }
 
+    public required BoardData Selection { get; init; }
+    public required BoardRenderer Parent { get; init; }
+    public required Position SelectionOffset { get; init; }
+    public required Position MouseOffset { get; init; }
+    public required Position Origin { get; init; }
+    public Position MousePosition => _mousePosition - MouseOffset;
+    private Position _mousePosition = mousePosition;
+
     public void OnClick(Position position) { }
 
     public void OnDragOver(Position position)
@@ -32,5 +35,11 @@ public sealed class MoveSelectionTool(Position mousePosition) : ITool
         _mousePosition = position;
     }
 
-    public void OnDrop(Position position) { }
+    public void OnDrop(Position position)
+    {
+        var removing = Selection.Tiles.Select(pos => pos + Origin - SelectionOffset);
+        Parent.Board.RemoveTiles(removing);
+        Parent.Board.AddAll(Selection, MousePosition - SelectionOffset);
+        Parent.Redraw();
+    }
 }
