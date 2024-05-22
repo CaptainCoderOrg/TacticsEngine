3254c16926fbfee013756c2dbd4c1db38a00197b
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-22 05:23:11 -0700

    feat: Implement CreateTilesCommand.

---
+++ CaptainCoder.TacticsEngine.Editor/CreateTilesCommand.cs
@@ -0,0 +1,15 @@
+﻿using CaptainCoder.TacticsEngine.Board;
+
+namespace CaptainCoder.TacticsEngine.Editor;
+
+public record CreateTilesCommand(BoardData Board, BoundingBox Selection) : IBoardEditCommand
+{
+    private readonly BoardData _originalBoard = Board.Copy();
+    public BoardData Do()
+    {
+        Board.CreateEmptyTiles(Selection.Positions());
+        return Board;
+    }
+
+    public BoardData Undo() => _originalBoard;
+}
---
+++ Tests/BoardEditor/CreateTilesCommand_should.cs
@@ -0,0 +1,55 @@
+﻿using CaptainCoder.TacticsEngine.Board;
+
+using Shouldly;
+
+using Tests;
+
+namespace CaptainCoder.TacticsEngine.Editor.Tests;
+
+public class CreateTilesCommand_should
+{
+    [Fact]
+    public void add_tiles_on_do()
+    {
+        // Arrange
+        BoardData emptyBoard = new();
+        CreateTilesCommand underTest = new(emptyBoard, new BoundingBox(3, 4, 3, 2));
+
+        // Act
+        BoardData updatedBoard = underTest.Do();
+
+        // Assert
+        updatedBoard.Tiles.Count.ShouldBe(6);
+        Position[] expectedTiles = [
+            new Position(3, 4),
+            new Position(4, 4),
+            new Position(5, 4),
+            new Position(3, 5),
+            new Position(4, 5),
+            new Position(5, 5),
+        ];
+        updatedBoard.Tiles.ShouldBeSubsetOf(expectedTiles);
+    }
+
+    [Fact]
+    public void remove_tiles_on_undo()
+    {
+        //Arrange
+        BoardData startingBoard = new()
+        {
+            Tiles = [.. new BoundingBox(1, 2, 3, 2).Positions()],
+        };
+
+        BoardData originalState = new()
+        {
+            Tiles = [.. new BoundingBox(1, 2, 3, 2).Positions()],
+        };
+
+        CreateTilesCommand underTest = new(startingBoard, new BoundingBox(3, 2, 2, 3));
+        _ = underTest.Do();
+
+        // Act
+        BoardData actual = underTest.Undo();
+        actual.ShouldBe(originalState, ErrorMessages.BoardCompareError(originalState, actual));
+    }
+}
