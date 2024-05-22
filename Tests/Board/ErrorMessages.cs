c2557ccc811299eeca3839a80f04b9a693775b53
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-05-22 05:22:18 -0700

    chore: Add ErrorMessages utility class.

---
+++ Tests/Board/ErrorMessages.cs
@@ -0,0 +1,20 @@
+﻿using CaptainCoder.Tactics.Board;
+using CaptainCoder.TacticsEngine.Board;
+
+namespace Tests;
+
+internal static class ErrorMessages
+{
+    public static string BoardCompareError(BoardData expected, BoardData actual)
+    {
+        return $"""
+         Expected:
+
+         {expected.ToAscii()}
+
+         But was:
+
+         {actual.ToAscii()}
+         """;
+    }
+}
